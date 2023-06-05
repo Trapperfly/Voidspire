a18a1fcc0e723e4d8c72cabb42f4606546cf4260
Gabriel Spangelid Johnsbråten <trapperfly@gmail.com>
2023-06-05 19:02:31 +0200

    Update gitignore and some shit i might regret

--- Assets/Scripts/GunShoot.cs
+++ Assets/Scripts/GunShoot.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe3208a07660d7a121fc294de8e652d2a283f3051165f01e679463160b800789
-size 628
+oid sha256:8b5cb8bdc5417e572ac3873cb64267998d472eebdf1def051577fb3a65a08944
+size 722
