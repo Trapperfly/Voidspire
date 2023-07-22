eb8eea503de15dab594c911e587437dc2b9a0b5f
Gabriel Spangelid Johnsbråten <trapperfly@gmail.com>
2023-07-22 14:36:57 +0200

    Corrected explosion trajectory

--- Assets/SpawnResourceOnDestroy.cs
+++ Assets/SpawnResourceOnDestroy.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ed4d734659f8752ac81e1f111e1b961e402adad752dbe761e4e7a82ce661082
-size 1569
+oid sha256:6cb822cc6e4539b705adaf75cefecf83c39936795d055a41d1739cfd47368bff
+size 2318
