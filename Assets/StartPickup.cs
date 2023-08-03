5aeb1144e81dbc583fc01bdd604ce5861f384dfb
Gabriel Spangelid Johnsbråten <trapperfly@gmail.com>
2023-08-03 12:52:54 +0200

    Build 0.0.5 Hotfix 1
    Fixed rouge and far too determined grabbers.

--- Assets/StartPickup.cs
+++ Assets/StartPickup.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f216827ec4b17c868f0aece2f56828ab29014134dbb670daff0095a47eec11bb
-size 4007
+oid sha256:11be30f5d9ec02e1e08bb48f6fd8f97e2706b056784298a6a13b3c3b1b8b43f1
+size 4020
