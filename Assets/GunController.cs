5f32e2fed1f8d48e232dde08c013ed359e087abd
Gabriel Spangelid Johnsbråten <trapperfly@gmail.com>
2023-08-10 23:20:14 +0200

    Gun rework (not working currently)

---
+++ Assets/GunController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c91b35e14ac72d4156070d33aec52b58365de5c4c41f50a8f4b2db013b194316
+size 5551
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81b06092e02d442259aec8a9d05e022a9c7a5c9103faebcb569bc97b3d593b84
-size 10341
+oid sha256:a46bf3876a1f6fada2a792c17bd29153eb9d1d0c077b37e929d3b9c06f7bce51
+size 9018
