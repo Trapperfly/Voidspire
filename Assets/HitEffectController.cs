3a4225664cbe4216be7af31443e5b4f0fcb8cb79
Gabriel Spangelid Johnsbråten <trapperfly@gmail.com>
2023-10-13 10:00:55 +0200

    Fixed Lurker prefab

--- Assets/HitEffectController.cs
+++ Assets/HitEffectController.cs
@@ -6,16 +6,22 @@ using UnityEngine.Sprites;
 public class HitEffectController : MonoBehaviour
 {
     [SerializeField] float lifeTime;
-    float currentDiffuse;
+    [SerializeField] bool fadeOut;
+    [SerializeField] float size;
+    [SerializeField] bool changeSizeOVerLifetime;
+    [SerializeField] AnimationCurve sizeOverLifetime;
+    float effectValue;
     Material mat;
     [SerializeField] Color32 BaseColor;
     [SerializeField] Color32 EffectColor;
     [SerializeField] Color32 EmissionColor;
+    float effectPercent;
 
     private void Awake()
     {
+        transform.localScale *= size;
         mat = GetComponent<SpriteRenderer>().material;
-        currentDiffuse = 60 * lifeTime;
+        effectValue = 60 * lifeTime;
         mat.SetColor("_BaseColor", BaseColor);
         mat.SetColor("_EffectColor", EffectColor);
         mat.SetColor("_EmissionColor", EmissionColor);
@@ -23,9 +29,21 @@ public class HitEffectController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        float diffusePercent = currentDiffuse / (60 * lifeTime);
-        mat.SetFloat("_Diffuse", diffusePercent);
-        currentDiffuse--;
-        if (currentDiffuse <= 0) Destroy(gameObject);
+        if (fadeOut)
+        {
+            effectPercent = effectValue / (60 * lifeTime);
+            mat.SetFloat("_Diffuse", effectPercent);
+        }
+        if (changeSizeOVerLifetime)
+        {
+            transform.localScale = new Vector3(size, size, 0) * CurveOverTime(sizeOverLifetime);
+        }
+        effectValue--;
+        if (effectValue <= 0) Destroy(gameObject);
+    }
+
+    float CurveOverTime(AnimationCurve curve)
+    {
+        return curve.Evaluate(1f - effectPercent);
     }
 }
