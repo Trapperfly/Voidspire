2b11f37efe9d26ee1a486935d0dcf96af89bd22d
Gabriel Spangelid Johnsbråten <trapperfly@gmail.com>
2023-11-03 13:34:24 +0100

    Start of death explosn effect

--- Assets/Scripts/Attributes/Destructable.cs
+++ Assets/Scripts/Attributes/Destructable.cs
@@ -5,9 +5,24 @@ using UnityEngine;
 public class Destructable : MonoBehaviour
 {
     [SerializeField] float delay = 0f;
+    [SerializeField] bool addParticleEffect;
+    [SerializeField] GameObject psPrefab;
+    [SerializeField] float psLifetime;
+    ParticleSystem ps;
     public IEnumerator DestroyMe()
     {
+        if (addParticleEffect) ps = Instantiate(psPrefab, transform).GetComponent<ParticleSystem>();
         Destroy(gameObject, delay);
         yield return null;
+    }
+    private void OnDestroy()
+    {
+        if (addParticleEffect)
+        {
+            var emis = ps.emission;
+            emis.enabled = false;
+            ps.transform.parent = null;
+            Destroy(ps.gameObject, psLifetime);
+        }
     }
 }
